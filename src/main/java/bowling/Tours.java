c9d8a396653dcb661012ee6b4cd0cb4b9f80a04b
CamilleKhv <camille.kerherve@gmail.com>
2023-11-16 16:02:22 +0100

    essais test bowling

--- src/main/java/bowling/Tours.java
+++ src/main/java/bowling/Tours.java
@@ -36,5 +36,21 @@ public class Tours {
 			res= true;
 		return res;
 	}
+	public ArrayList<Lancer> getLancer() {
+		return lesLancers;
+	}
+	public double bonus(){
+		double ret=0;
+		Tours tourprecedent=new Tours(num-1);
+		//fais un spare
+		if(estUnSpare()==true) {
+			ret=lesLancers.get(0).getnbQuillesTombees();
+		}
+		//fais un strike
+		if(estUnStrike()==true){
+			ret=lesLancers.get(0).getnbQuillesTombees()+lesLancers.get(1).getnbQuillesTombees();
+		}
+		return ret;
+	}
 
 }
